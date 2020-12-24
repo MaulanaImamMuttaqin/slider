c58c3abba249f06888b8aa71abb8e59852c7ae20
Maulana Imam Muttaqin <maulanaimammuttaqin2000@gmail.com>
2020-12-24 21:16:54 +0700

    finish

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 let images = document.querySelectorAll(".wrapper img"),
+    wrapper = document.querySelector(".wrapper"),
     prev = document.querySelector("#left"),
     next = document.querySelector("#right"),
-    slidePosition= 0,
-    firtSlide = images[0],
-    lastChild = images[images.length] ,
-    slideWidth = firtSlide.getBoundingClientRect().width;
+    slideWidth= images[0].getBoundingClientRect().width,
+    slidePosition = 0;
 
-// prev.addEventListener("click", function(){shiftSlide(-1)})
-// next.addEventListener("click", function(){shiftSlide(1)})
-
+prev.addEventListener("click", function(){shiftSlide(-1)})
+next.addEventListener("click", function(){shiftSlide(1)})
 
 function shiftSlide(dirc){
+
     if(dirc > 0 ){
         slidePosition = slidePosition + slideWidth
-
+        if(slidePosition === slideWidth*images.length){
+            slidePosition = 0
+        }
     }
     else{
         slidePosition = slidePosition - slideWidth
+        if(slidePosition < 0 ){
+            slidePosition = slideWidth*(images.length-1)
+        }
     }
-    
-    images.forEach(image => {
-        image.style.right = slidePosition +"px"
-    })
-    
-    console.log(slidePosition)
+    wrapper.style.right = slidePosition +"px"
 }
+
